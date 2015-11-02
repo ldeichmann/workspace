a8eb91913831c13e728b43bd0f506483c6ccffbc
Lukas Deichmann <github@pezam.de>
2015-11-02 12:49:29 +0100

    Aufgabe 3

--- Rechnernetze_02/src/Aufgabe3_FileTransferServerUDPjlibcnds.java
+++ Rechnernetze_02/src/Aufgabe3_FileTransferServerUDPjlibcnds.java
@@ -1,6 +1,5 @@
 //package experiment2;
 
-import java.lang.reflect.Array;
 import java.net.*; // we use Sockets
 import java.util.Arrays;
 
@@ -27,25 +26,33 @@ public class Aufgabe3_FileTransferServerUDPjlibcnds {
 	    DatagramPacket packet = new DatagramPacket(buf, buf.length);
 	    
 	    java.io.FileInputStream fr = new java.io.FileInputStream(comparisonFilename);
-	    int len, err = 0;
+	    int len = 0;
+	    int err = 0;
 	    
 	    
 	    while (true){
 		    dtgSock.receive(packet);
 //		    System.out.print("*");
 		    // if receive an empty packet will indicate end of file
-        	System.out.println(Arrays.toString(packet.getData()));
+//        	System.out.println(Arrays.toString(packet.getData()));
 		
 		    if (packet.getLength()==0) break;
 //            fw.write(packet.getData(),0,packet.getLength());
             if ((len = fr.read(inBuf, 0, inBuf.length)) != -1) {
-            	System.out.println(Arrays.toString(inBuf));
+//            	System.out.println(Arrays.toString(inBuf));
+            	int nerr = err;
             	err = err + Tools.countBitErrorsInByteArray(inBuf, packet.getData());
+            	if (nerr < err ) {
+                	System.out.print(new String(inBuf));
+                	System.out.print(new String(packet.getData()));
+                	System.out.println("---");
+            	}
             }
 		    //fw.flush();
 	    }
 	    fw.flush();
 	    fw.close();
+	    fr.close();
 	    dtgSock.close();	// Close the Socket
 	    System.out.println("");
 	    System.out.println("Errors: " + err);

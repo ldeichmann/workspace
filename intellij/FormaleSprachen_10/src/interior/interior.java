9bae5299f5f302a6bdf1c90644d8b209800a4268
Lukas Deichmann <lukasdeichmann+github@gmail.com>
2017-01-17 00:24:16 +0100

    FS 10

---
+++ intellij/FormaleSprachen_10/src/interior/interior.java
@@ -0,0 +1,109 @@
+package interior;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import cymbol.CymbolBaseListener;
+import cymbol.CymbolLexer;
+import cymbol.CymbolParser;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeProperty;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+public class interior {
+
+    public static class ComplexityCounter extends CymbolBaseListener {
+
+        ParseTreeProperty<Integer> values = new ParseTreeProperty<>();
+
+        @Override
+        public void exitFunctionDecl(CymbolParser.FunctionDeclContext ctx) {
+            int val = 0;
+            for (int i = 0; i < ctx.getChildCount(); i++) {
+                if (values.get(ctx.getChild(i)) != null) {
+                    val = val + getValue(ctx.getChild(i));
+                }
+            }
+            System.out.println("Function: " + ctx.ID().toString());
+            System.out.println("Complexity: " + val);
+        }
+
+        @Override
+        public void exitBlock(CymbolParser.BlockContext ctx) {
+            int val = 0;
+            for (int i = 0; i < ctx.getChildCount(); i++) {
+                if (values.get(ctx.getChild(i)) != null) {
+                    val = val + getValue(ctx.getChild(i));
+                }
+            }
+            setValue(ctx, val);
+        }
+
+        @Override
+        public void exitStat(CymbolParser.StatContext ctx) {
+//            System.out.println(ctx.getChild(0));
+            if (ctx.getChild(0).getText().equals("for")) {
+//                System.out.println(ctx.getChild(0));
+                setValue(ctx, 2);
+            }
+            else if (ctx.getChild(0).getText().equals("if")) {
+//                System.out.println(ctx.getChild(0));
+                int pow = 0;
+                int elses = 0;
+                for (int i = 1; i < ctx.getChildCount(); i++) {
+                    if (values.get(ctx.getChild(i)) != null) {
+                        pow++;
+                    } else
+                    if (ctx.getChild(i).getText().equals("else")) {
+                        elses++;
+                    }
+                }
+                setValue(ctx, (int)Math.pow(2, pow) + elses);
+            } else {
+                int val = 0;
+                if (values.get(ctx) != null) {
+                    val = getValue(ctx);
+                }
+                for (int i = 0; i < ctx.getChildCount(); i++) {
+                    if (values.get(ctx.getChild(i)) != null) {
+                        val = val + getValue(ctx.getChild(i));
+                    }
+                }
+                setValue(ctx, val);
+            }
+        }
+
+        public void setValue(ParseTree node, int value) {
+            values.put(node, value);
+        }
+
+        public int getValue(ParseTree node) {
+            return values.get(node);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        String inputFile = null;
+        if (args.length > 0) inputFile = args[0];
+        InputStream is = System.in;
+        if (inputFile != null) {
+            is = new FileInputStream(inputFile);
+        }
+        ANTLRInputStream input = new ANTLRInputStream(is);
+        CymbolLexer lexer = new CymbolLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        CymbolParser parser = new CymbolParser(tokens);
+        parser.setBuildParseTree(true);
+        ParseTree tree = parser.file();
+
+//        System.out.println(tree.toStringTree(parser));
+
+        ParseTreeWalker walker = new ParseTreeWalker();
+        ComplexityCounter ccount = new ComplexityCounter();
+        walker.walk(ccount, tree);
+//        System.out.println("complexity = " + ccount.getValue(tree));
+    }
+
+}
---
+++ intellij/FormaleSprachen_10/src/mccabe/mccabe.java
@@ -0,0 +1,104 @@
+package mccabe;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import cymbol.CymbolBaseListener;
+import cymbol.CymbolLexer;
+import cymbol.CymbolParser;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeProperty;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+public class mccabe {
+
+    public static class ComplexityCounter extends CymbolBaseListener {
+
+        ParseTreeProperty<Integer> values = new ParseTreeProperty<>();
+
+        @Override
+        public void exitFunctionDecl(CymbolParser.FunctionDeclContext ctx) {
+            int val = 0;
+            for (int i = 0; i < ctx.getChildCount(); i++) {
+                if (values.get(ctx.getChild(i)) != null) {
+                    val = val + getValue(ctx.getChild(i));
+                }
+            }
+            System.out.println("Function: " + ctx.ID().toString());
+            System.out.println("Complexity: " + (val+1));
+        }
+
+        @Override
+        public void exitBlock(CymbolParser.BlockContext ctx) {
+            int val = 0;
+            for (int i = 0; i < ctx.getChildCount(); i++) {
+                if (values.get(ctx.getChild(i)) != null) {
+                    val = val + getValue(ctx.getChild(i));
+                }
+            }
+            setValue(ctx, val);
+        }
+
+        @Override
+        public void exitStat(CymbolParser.StatContext ctx) {
+//            System.out.println(ctx.getChild(0));
+            if (ctx.getChild(0).getText().equals("for")) {
+//                System.out.println(ctx.getChild(0));
+                setValue(ctx, 2);
+            }
+            else if (ctx.getChild(0).getText().equals("if")) {
+//                System.out.println(ctx.getChild(0));
+                setValue(ctx, 1);
+                for (int i = 0; i < ctx.getChildCount(); i++) {
+                    if (ctx.getChild(i).getText().equals("else")) {
+                        setValue(ctx, getValue(ctx) + 1);
+//                        System.out.println(ctx.getChild(i));
+                    }
+                }
+            }
+            int val = 0;
+            if (values.get(ctx) != null) {
+                val = getValue(ctx);
+            }
+            for (int i = 0; i < ctx.getChildCount(); i++) {
+                if (values.get(ctx.getChild(i)) != null) {
+                    val = val + getValue(ctx.getChild(i));
+                }
+            }
+            setValue(ctx, val);
+        }
+
+        public void setValue(ParseTree node, int value) {
+            values.put(node, value);
+        }
+
+        public int getValue(ParseTree node) {
+            return values.get(node);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        String inputFile = null;
+        if (args.length > 0) inputFile = args[0];
+        InputStream is = System.in;
+        if (inputFile != null) {
+            is = new FileInputStream(inputFile);
+        }
+        ANTLRInputStream input = new ANTLRInputStream(is);
+        CymbolLexer lexer = new CymbolLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        CymbolParser parser = new CymbolParser(tokens);
+        parser.setBuildParseTree(true);
+        ParseTree tree = parser.file();
+
+//        System.out.println(tree.toStringTree(parser));
+
+        ParseTreeWalker walker = new ParseTreeWalker();
+        ComplexityCounter ccount = new ComplexityCounter();
+        walker.walk(ccount, tree);
+//        System.out.println("complexity = " + ccount.getValue(tree));
+    }
+
+}
